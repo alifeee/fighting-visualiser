b94866a4b8bcaca19e8af4a757e0a37f680163ee
alifeee <alifeee@alifeee.net>
2025-07-09 16:27:04 +0100

    add fading health bar on hit

--- scripts/health_bar.gd
+++ scripts/health_bar.gd
@@ -5,6 +5,8 @@ signal zeroed()
 
 const FLASH_WHITE_TIME = 0.15
 const BULGE_TIME = 0.15
+const FADE_HEALTH_FROM = 0.5
+const FADE_HEALTH_TIME = 1.75
 
 @export var swap: bool = false
 @export_range(0, 100, 0.01) var health: float = 100:
@@ -35,7 +37,7 @@ func _on_health_changed(health):
 	# do nothing - health bar continually follows health
 	pass
 
-func _on_hit(health: float) -> void:
+func _on_hit(health: float) -> void:	
 	var tween = get_tree().create_tween()
 	# flash white
 	tween.set_parallel().tween_property(
@@ -45,4 +47,16 @@ func _on_hit(health: float) -> void:
 	tween.tween_property(
 		self, "scale", Vector2(1,1), BULGE_TIME
 	).from(Vector2(1.1,1.1))
+	
+	# ghost bar
+	var ghost_bar: ProgressBar = progressbar.duplicate()
+	progressbar.get_parent().add_child(ghost_bar)
+	progressbar.get_parent().move_child(ghost_bar,0)
+	# fade and delete
+	var fadetween = get_tree().create_tween()
+	fadetween.tween_property(
+		ghost_bar, "modulate:a",0,FADE_HEALTH_TIME
+	).from(FADE_HEALTH_FROM)
+	fadetween.tween_callback(func (): ghost_bar.queue_free())
+	
 	self.health -= health
